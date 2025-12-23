b31a519c8b34e80c31d79d7e17271a4c1a615cff
ian-shakespeare <ian@shakespeare.dev>
2025-12-22 23:12:15 -0700

    separate nav icons

---
+++ mobile/src/components/MapIcon.tsx
@@ -0,0 +1,5 @@
+import { Icon, IconElement, IconProps } from "@ui-kitten/components";
+
+export default function MapIcon(props: IconProps): IconElement {
+  return <Icon {...props} name="map-outline" />;
+}
---
+++ mobile/src/components/PersonIcon.tsx
@@ -0,0 +1,5 @@
+import { Icon, type IconProps } from "@ui-kitten/components";
+
+export default function PersonIcon(props: IconProps) {
+  return <Icon {...props} name="person-outline" />;
+}
