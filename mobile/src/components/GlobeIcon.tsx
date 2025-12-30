c32ff03289600769d40e988440188d6d4ef3269e
ian-shakespeare <ian@shakespeare.dev>
2025-12-29 19:58:07 -0700

    add new icons

---
+++ mobile/src/components/GlobeIcon.tsx
@@ -0,0 +1,5 @@
+import { Icon, type IconProps } from "@ui-kitten/components";
+
+export default function GlobeIcon(props: IconProps) {
+  return <Icon {...props} name="globe-2-outline" />;
+}
---
+++ mobile/src/components/ImageIcon.tsx
@@ -0,0 +1,5 @@
+import { Icon, type IconProps } from "@ui-kitten/components";
+
+export default function ImageIcon(props: IconProps) {
+  return <Icon {...props} name="image-outline" />;
+}
