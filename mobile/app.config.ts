52e2debc57129f299354ab65ef4c3f5eccd64146
Ian Shakespeare <irshakes11@gmail.com>
2026-09-25 22:32:36 +0900

    bump version, fix bug

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Tribe Tracker",
   slug: "tribe-tracker",
-  version: "1.0.1",
+  version: "1.1.0",
   orientation: "portrait",
   userInterfaceStyle: "dark",
   scheme: "tribetracker",
